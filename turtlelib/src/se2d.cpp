80c4524e5984197e29e4614e786764bf0655f050
Stella <jialuyu2024@u.northwestern.edu>
2024-01-20 16:06:38 -0600

    added B.6 PARTIALLY

--- turtlelib/src/se2d.cpp
+++ turtlelib/src/se2d.cpp
@@ -38,7 +38,7 @@ namespace turtlelib
             {}
 
 
-        Transform2D::Transform2D{Vector2D trans}:
+        Transform2D::Transform2D(Vector2D trans):
             linear{trans},
             angular{0}
             {}
@@ -54,37 +54,37 @@ namespace turtlelib
             angular{radians}
             {}
 
-        Transform2D::Point2D operator()(Point2D p) const{
+        Point2D Transform2D::operator()(Point2D p) const{
             Point2D newPt;
             newPt.x = p.x*cos(angular)-sin(angular)*p.y+linear.x;
             newPt.y = p.x*sin(angular)+cos(angular)*p.y+linear.y;
             return newPt;
         }
 
-        Transform2D::Vector2D operator()(Vector2D v) const{
+        Vector2D Transform2D::operator()(Vector2D v) const{
             const auto new_x = v.x*cos(angular)-sin(angular)*v.y; //for vector there is no translation
             const auto new_y = v.x*sin(angular)+cos(angular)*v.y;
             return Vector2D{new_x, new_y};
         }
 
-        Transform2D::Twist2D operator()(Twist2D v) const{
+        Twist2D Transform2D::operator()(Twist2D v) const{
             Twist2D newTwist;
-            newTwist.x = v.x;
-            newTwist.y = v.x*linear.y + v.y*cos(angular)-v.omega*sin(angular);
-            newTwist.omega = -v.x*linear.x + v.y*sin(angular) + v.omega*cos(angular);
+            newTwist.x = v.omega*linear.y + v.x*cos(angular) - v.y*sin(angular);
+            newTwist.y = v.omega*(-linear.x) + v.x*sin(angular)+ v.y*cos(angular);
+            newTwist.omega = v.omega;
             return newTwist;
         }
 
-        Transform2D::Transform2D inv() const{
+        Transform2D Transform2D::inv() const{
             Transform2D newTrans;
-            newTrans.linear.x = -x*cos(angular)-y*sin(angular);
-            newTrans.linear.y = -y*cos(angular)+x*sin(angular);
+            newTrans.linear.x = -linear.x*cos(angular)-linear.y*sin(angular);
+            newTrans.linear.y = -linear.y*cos(angular)+linear.x*sin(angular);
             auto oldAngular = angular;
             newTrans.angular = -oldAngular;
             return newTrans;
         }
 
-        Transform2D::Transform2D & Transform2D::operator*=(const Transform2D & rhs){
+        Transform2D & Transform2D::operator*=(const Transform2D & rhs){
             const auto new_angular = angular + rhs.angular;
             const auto new_x = rhs.linear.x*cos(angular) - rhs.linear.y*sin(angular) + linear.x;
             const auto new_y = rhs.linear.x*sin(angular) + rhs.linear.y*cos(angular) + linear.y;
@@ -95,12 +95,12 @@ namespace turtlelib
             return *this;
         }
 
-        Transform2D::Vector2D translation() const{
+        Vector2D Transform2D::translation() const{
             return linear;
         }
 
 
-        Transform2D::double rotation() const{
+        double Transform2D::rotation() const{
             return angular;
         }
 
@@ -108,7 +108,7 @@ namespace turtlelib
 
 
     std::ostream & operator<<(std::ostream & os, const Transform2D & tf){
-        os << "deg: " << tf.angular <<  " x: " << tf.linear.x << " y: " << tf.linear.y; 
+        os << "deg: " << rad2deg(tf.angular) <<  " x: " << tf.linear.x << " y: " << tf.linear.y; 
     }
 
 
@@ -117,15 +117,17 @@ namespace turtlelib
         is >> std::ws;
         double ang_val, x_val, y_val;
         std::string label_ang, label_x, label_y;
-        if(is.peek()=="d"){
+        char c = is.peek();
+        if(c =='d'){
             is >> label_ang >> ang_val >> label_x >> x_val >> label_y >> y_val;
-        }else{
-            is >> input_ang >> input_x >> input_y;
+        }
+        else{
+            is >> ang_val >> x_val >> y_val;
         }
         // Read \n character?????????????????
         c = is.get(); 
-        
-        tf = Transform2D(Vector2D{input_x, input_y},deg2rad(input_ang));
+        tf = Transform2D(Vector2D{x_val, y_val},deg2rad(ang_val));
+
         return is;
     }
 
@@ -134,6 +136,4 @@ namespace turtlelib
         return lhs*=rhs;
     }
 
-};
-
-#endi
+}

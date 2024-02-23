e0be7245c9f8eed3f9295a17153aae362cab41a6
Stella <jialuyu2024@u.northwestern.edu>
2024-02-23 13:08:45 -0600

    C.10 achieved

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -42,6 +42,7 @@
 #include "turtlelib/diff_drive.hpp"
 #include "turtlelib/geometry2d.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include <random>
 
 using namespace std::chrono_literals;
 
@@ -55,6 +56,10 @@ public:
     declare_parameter("rate", 200.);
     rate_hz = get_parameter("rate").as_double();
     std::chrono::milliseconds rate = (std::chrono::milliseconds) ((int)(1000. / rate_hz));
+
+    declare_parameter("rate_fakesensor", 5);
+    rate_fakesensor_hz = get_parameter("rate_fakesensor").as_int();
+    std::chrono::milliseconds rate_fakesensor = (std::chrono::milliseconds) ((int)(1000. / rate_fakesensor_hz));
 
     declare_parameter("x", 0.);
     x_i = get_parameter("x").as_double();
@@ -80,6 +85,9 @@ public:
     declare_parameter("obstacles/r", 0.0);
     obr = get_parameter("obstacles/r").as_double();
 
+    declare_parameter("collision_radius", 0.0);
+    collision_radius = get_parameter("collision_radius").as_double();
+
     declare_parameter("motor_cmd_per_rad_sec", 0.0);
     motor_cmd_per_rad_sec = get_parameter("motor_cmd_per_rad_sec").as_double();
 
@@ -101,13 +109,33 @@ public:
         RCLCPP_DEBUG_STREAM(get_logger(), "encoder_ticks_per_rad error" << 4);
     }
 
+    declare_parameter("draw_only", false);
+    draw_only = get_parameter("draw_only").as_bool();
+    ///gausion noise with variance input_noise
+    declare_parameter("input_noise", 0.0);
+    input_noise = get_parameter("input_noise").as_double();
+    ///slip fraction
+    declare_parameter("slip_fraction", 0.0);
+    slip_fraction = get_parameter("slip_fraction").as_double();
+    ///gausion noise for sensor_variance
+    declare_parameter("basic_sensor_variance", 0.01);
+    basic_sensor_variance = get_parameter("basic_sensor_variance").as_double();
+
     
+
+
+
     initial = true;
     x0 = 0.0;
     y0 = 0.0;
     theta0 = 0.0;
+    /// timer for normal publishing wall stuff
     timer_ = create_wall_timer(
       rate, std::bind(&Nusim::timer_callback, this));
+    /// timer for fake sensor publishing
+    timer2_ = create_wall_timer(
+      rate_fakesensor, std::bind(&Nusim::timer_callback_slow, this));
+
     //timestep publisher
     time_step_publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
     //service reset
@@ -139,19 +167,13 @@ public:
     rclcpp::QoS qos_policy = rclcpp::QoS(rclcpp::KeepLast(10)).transient_local();
     wall_pub = create_publisher<visualization_msgs::msg::MarkerArray>("~/walls", qos_policy);
     obs_pub = create_publisher<visualization_msgs::msg::MarkerArray>("~/obstacles", qos_policy);
+    fake_sensor_cyl_pub = create_publisher<visualization_msgs::msg::MarkerArray>("~/fake_sensor", qos_policy);
     
   }
 
 private:
   void timer_callback()
   {
-    //if its the first time calling initial, set the initial location
-    if (initial) {
-      x0 = x_i;
-      y0 = y_i;
-      theta0 = theta_i;
-      initial = false;
-    }
     // publish timetsep
     auto message = std_msgs::msg::UInt64();
     message.data = timestep_;
@@ -162,32 +184,55 @@ private:
     wallPub();
     obstacle_publisher();
 
-    ///update the robot configuration
-    auto delta_left_angle = left_velocity/rate_hz;///velocity(rad/s)*time to get rad
-    auto delta_right_angle = right_velocity/rate_hz;
-    auto new_right_angle = delta_right_angle + robot.get_wheel_state().r;
-    auto new_left_angle = delta_left_angle + robot.get_wheel_state().l;
-    ///use delta wheel degree to update wheel state
-    robot.forwardKinematics(turtlelib::WheelState{delta_left_angle, delta_right_angle});
+    if(!draw_only){
+      //if its the first time calling initial, set the initial location
+      if (initial) {
+        x0 = x_i;
+        y0 = y_i;
+        theta0 = theta_i;
+        initial = false;
+      }
+      ///update the robot configuration
+      auto delta_left_angle = left_velocity/rate_hz;///velocity(rad/s)*time to get rad
+      auto delta_right_angle = right_velocity/rate_hz;
+      ///QUESTIONS?? WHY LEFT ARE USING U*(1+slip_fraction)???
+      delta_left_angle = delta_left_angle * (1 + slip_noise);
+      delta_right_angle = delta_right_angle * (1 + slip_noise);
 
-    x = robot.get_current_config().translation().x;
-    y = robot.get_current_config().translation().y;
-    theta = robot.get_current_config().rotation();
+      auto new_right_angle = delta_right_angle + robot.get_wheel_state().r;
+      auto new_left_angle = delta_left_angle + robot.get_wheel_state().l;
 
-    send_transform(x, y, theta);
-    //update the sensor call back
-    sensor_data.left_encoder = static_cast<int>(new_left_angle*encoder_ticks_per_rad);
-    sensor_data.right_encoder = static_cast<int>(new_right_angle*encoder_ticks_per_rad);
-    sensor_data.stamp = get_clock()->now();
-    sensor_data_pub->publish(sensor_data);
+      ///use delta wheel degree to update wheel state
+      robot.forwardKinematics(turtlelib::WheelState{delta_left_angle, delta_right_angle});
 
-    //publish the nav_msgs::msg::Path
-    red_path.header.frame_id = "nusim/world";
-    red_path.header.stamp = get_clock()->now();
-    path_pub->publish(red_path);
+      x = robot.get_current_config().translation().x;
+      y = robot.get_current_config().translation().y;
+      theta = robot.get_current_config().rotation();
+
+      send_transform(x, y, theta);
+      //update the sensor call back
+      sensor_data.left_encoder = static_cast<int>(new_left_angle*encoder_ticks_per_rad);
+      sensor_data.right_encoder = static_cast<int>(new_right_angle*encoder_ticks_per_rad);
+      sensor_data.stamp = get_clock()->now();
+      sensor_data_pub->publish(sensor_data);
+
+      //publish the nav_msgs::msg::Path
+      red_path.header.frame_id = "nusim/world";
+      red_path.header.stamp = get_clock()->now();
+      path_pub->publish(red_path);
+    }
   }
 
+  void timer_callback_slow()
+  {
+    //publish the sensor data on fake obstacles
+    if(!draw_only){
+      fake_sensor_cyl_publisher();
+      // RCLCPP_INFO_STREAM(get_logger(), "Fake sensor publishing");
+    }
 
+
+  }
   void reset(
     std::shared_ptr<std_srvs::srv::Empty::Request>,
     std::shared_ptr<std_srvs::srv::Empty::Response>)
@@ -329,21 +374,119 @@ private:
     obs_pub->publish(arr_obstacle);
   }
 
+  void fake_sensor_cyl_publisher(){
+    if (oby.size() == obx.size()) {
+      RCLCPP_INFO_ONCE(get_logger(), "Valid Marker Input!");
+      cyl_num = oby.size();
+    } else {
+      RCLCPP_DEBUG(get_logger(), "Invalid Marker Input!");
+      cyl_num = 0;
+      return;
+    }
+    visualization_msgs::msg::MarkerArray arr_fake_sensor_obstacle;
+    RCLCPP_INFO_STREAM(get_logger(), "I get into fake sensor publisher");
+    for (size_t i = 0; i < cyl_num; i++) {
+      
+      RCLCPP_INFO_STREAM(get_logger(), "FOR LOOP TIME" << i);
+      visualization_msgs::msg::Marker m;
+      m.header.stamp = this->get_clock()->now();
+      m.header.frame_id = "red/base_footprint";
+      m.id = i;
+      if(dist_obs_robot(i, range)){
+        m.type = 3;
+        m.action = 0;
+
+        ///create gaussian noise on the obstacle location
+        std::normal_distribution<double> d(0, basic_sensor_variance);
+        auto obs_noise = d(get_random());
+        RCLCPP_INFO_STREAM(get_logger(), "obs_noise" << obs_noise);
+
+        m.color.r = 1.0;
+        m.color.g = 1.0;
+        m.color.b = 0.0;
+        m.color.a = 1.0;
+        //wall scale
+        m.scale.x = this->obr * 2 + obs_noise;
+        m.scale.y = this->obr * 2 + obs_noise;
+        m.scale.z = 0.25;
+        const turtlelib::Transform2D column_location{turtlelib::Vector2D {this->obx[i], this->oby[i]}, 0};
+        RCLCPP_INFO_STREAM(get_logger(), "COLUMN LOCATION TRC as" << column_location.translation().x << " " << column_location.translation().y << " " << column_location.rotation());
+        const auto robot_cur_location = robot.get_current_config(); ///also a Transform2D
+        ///return a vector of the translation, Trc = Trs * Tsc = Tsr.inv() * Tsc
+        const auto Trc_location = (robot_cur_location.inv() * column_location).translation();
+        m.pose.position.x = Trc_location.x + obs_noise;
+        m.pose.position.y = Trc_location.y + obs_noise;
+        m.pose.position.z = 0.125;
+        RCLCPP_INFO_STREAM(get_logger(), "new obstacle added");
+      }
+      else{
+        m.action = visualization_msgs::msg::Marker::DELETE;
+        
+        RCLCPP_INFO_STREAM(get_logger(), "delete the obstacle marker!");
+      }
+      arr_fake_sensor_obstacle.markers.push_back(m);
+    }
+    fake_sensor_cyl_pub->publish(arr_fake_sensor_obstacle);
+  }
+
   void wheel_callback(const nuturtlebot_msgs::msg::WheelCommands & wc)
   {
     // just store left and right velocity and do this update in the timer
     left_velocity = wc.left_velocity * motor_cmd_per_rad_sec;   //wc in ticks, get the velocity in rad/s
     right_velocity = wc.right_velocity * motor_cmd_per_rad_sec;
+    if(left_velocity != 0){
+      left_velocity += gaussian_noise_velocity;
+    }
+    if(right_velocity != 0){
+      right_velocity += gaussian_noise_velocity;
+    }
     // RCLCPP_INFO_STREAM(
     //   get_logger(), "Teleporting service as x=" << left_velocity << " y=" << right_velocity);
   }
 
+  std::mt19937 & get_random()
+  {
+      // static variables inside a function are created once and persist for the remainder of the program
+      static std::random_device rd{}; 
+      static std::mt19937 mt{rd()};
+      // we return a reference to the pseudo-random number genrator object. This is always the
+      // same object every time get_random is called
+      return mt;
+  }
+  void generate_noise(){
+    //generate the noise
+    std::normal_distribution<double> d(0, input_noise);
+    gaussian_noise_velocity = d(get_random());
+    std::uniform_real_distribution<> u(-slip_fraction, slip_fraction);
+    slip_noise = u(get_random());
+  }
+
+  bool dist_obs_robot(int i, double range){
+    double cyl_x = obx[i];
+    double cyl_y = oby[i];
+    auto robot_x = robot.get_current_config().translation().x;
+    auto robot_y = robot.get_current_config().translation().y;
+    auto dist = std::sqrt(std::pow(cyl_x - robot_x, 2) + std::pow(cyl_y - robot_y, 2));
+    if((range == 0) && ((dist - obr - collision_radius) < range)){
+      RCLCPP_INFO_STREAM(get_logger(), "return true, situation 1, the distance is inside range" << (dist - obr - collision_radius));
+      return true;
+    }
+    else if((dist - obr - collision_radius) < range){
+      RCLCPP_INFO_STREAM(get_logger(), "return true, the distance is inside range" << (dist - obr - collision_radius));
+      return true;
+    }
+    RCLCPP_INFO_STREAM(get_logger(), "return false" << (dist - obr - collision_radius));
+    return false;
+    
+  }
+
+
   double x_i, y_i, theta_i;
-  double x, y, theta, rate_hz, encoder_ticks_per_rad;
+  double x, y, theta, rate_hz, encoder_ticks_per_rad, rate_fakesensor_hz;
   double arena_x_length, arena_y_length;
   double track_width, wheel_radius, motor_cmd_per_rad_sec;
   std::vector<double> obx, oby;
-  double obr;
+  double obr, collision_radius, basic_sensor_variance;
   bool initial;
   double x0, y0, theta0;
   size_t timestep_;
@@ -352,11 +495,13 @@ private:
   double right_velocity=0.0;
   nav_msgs::msg::Path red_path; //the path of the robot
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::TimerBase::SharedPtr timer2_;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr time_step_publisher_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr obs_pub;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr wall_pub;
   rclcpp::Publisher<nuturtlebot_msgs::msg::SensorData>::SharedPtr sensor_data_pub;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr fake_sensor_cyl_pub;
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_;
@@ -364,6 +509,12 @@ private:
   turtlelib::DiffDrive robot{0.033, 0.16};
   nuturtlebot_msgs::msg::SensorData sensor_data;
   geometry_msgs::msg::PoseStamped robot_pose = geometry_msgs::msg::PoseStamped();
+  bool draw_only = false;
+  double input_noise = 0.0;
+  double slip_fraction = 0.0;
+  double gaussian_noise_velocity = 0.0;
+  double slip_noise = 0.0;
+  double range = 3.0; //maximum range of the fake sensor to detect the obstacle
 
 };
 
